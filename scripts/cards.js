bc905c55c1d6ca90a38ce0cb12bb6313120a6073
ArtPsycho <denhardrock24@gmail.com>
2023-10-24 00:33:12 +0300

    ver 1.0 / index.js, cards.js

--- scripts/cards.js
+++ scripts/cards.js
@@ -23,4 +23,8 @@ const initialCards = [
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     },
+    {
+      name: "Алтай",
+      link: "https://images.unsplash.com/photo-1593948360735-4ddb6f0dde7d?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
 ];
